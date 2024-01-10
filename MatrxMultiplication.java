a5f32e4db1ba62e898e6be3a6335193a5e6f3bad
veneela123 <113539505+veneela123@users.noreply.github.com>
2024-01-10 13:50:53 +0530

    Create MatrxMultiplication.java

---
+++ MatrxMultiplication.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    System.out.print("N:");  
+	    int n=s.nextInt();
+	    int a[][]=new int[n][n];
+	    int b[][]=new int[n][n];
+	    int c[][]=new int[n][n];
+	    System.out.println("Enter elements of A matrix");
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+    	    {
+    	        a[i][j]=s.nextInt();
+    	    }
+	    }
+	    System.out.println("Enter elements of A matrix");
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+    	    {
+    	        b[i][j]=s.nextInt();
+    	    }
+	    }
+	    System.out.println("Multiplication of matrix");
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+    	    {
+    	        for(int k=0;k<n;k++)
+    	        {
+    	            c[i][j]+=a[i][k]*b[k][j];
+    	        }
+    	        System.out.print(c[i][j]+" ");
+    	    }
+    	    System.out.println();
+	    }
+	}
+}
