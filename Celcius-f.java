d1d4f6a404d307a9ff8e03967e01239cb3f789f3
veneela123 <113539505+veneela123@users.noreply.github.com>
2024-01-10 11:18:50 +0530

    Create Celcius-f.java

---
+++ Celcius-f.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    System.out.print("C:");
+	    double C=s.nextDouble();
+		double F=((9/5)*C)+32;
+		System.out.println("F:"+F);
+	}
+}
