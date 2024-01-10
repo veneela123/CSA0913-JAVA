be9182a59c971ccd3b9e09d2d2bf6d314f0f7383
veneela123 <113539505+veneela123@users.noreply.github.com>
2024-01-10 11:42:57 +0530

    Create SquareArea.java

---
+++ SquareArea.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    System.out.print("N:");
+	    int side=s.nextInt();
+	    int area=side*side;
+	    System.out.println("Area of square:"+area);
+	}
+}
